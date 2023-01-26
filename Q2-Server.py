b501fcd7160641da1cb65580391db0753591ec5b
syahmina25 <85839558+syahmina25@users.noreply.github.com>
2023-01-26 15:23:59 +0800

    Update Q2-Server.py

--- Q2-Server.py
+++ Q2-Server.py
@@ -6,7 +6,7 @@ def fahrenheit_to_celsius(fahrenheit):
 
 def main():
     host = "127.0.0.1"
-    port = 12345
+    port = 8080
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((host, port))
