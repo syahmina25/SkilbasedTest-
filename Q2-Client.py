144d40da99192d5ddc055da359db5d6be168ada1
syahmina25 <85839558+syahmina25@users.noreply.github.com>
2023-01-26 14:24:18 +0800

    Update Q2-Client.py

--- Q2-Client.py
+++ Q2-Client.py
@@ -1,1 +1,20 @@
+import socket
+
+def main():
+    host = "127.0.0.1"
+    port = 12345
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+
+    fahrenheit = input("Enter temperature in Fahrenheit: ")
+    s.send(fahrenheit.encode())
+
+
+    celsius = s.recv(1024).decode()
+    print(f"Temperature in Celsius: {celsius}")
+    s.close()
+
+if __name__ == "_main_":
+    main()
 
