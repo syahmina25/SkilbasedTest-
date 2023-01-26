4f18762f10c9311503d2a8abb5dd82655d3b8ca6
syahmina25 <85839558+syahmina25@users.noreply.github.com>
2023-01-26 15:01:07 +0800

    Update Q3-Client.py

--- Q3-Client.py
+++ Q3-Client.py
@@ -1,1 +1,15 @@
+import socket
 
+def main():
+    host = "127.0.0.1"
+    port = 8888
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+
+    quote = s.recv(1024).decode()
+    print(f"Quote of the Day: {quote}")
+    s.close()
+
+if __name__ == "_main_":
+    main()
